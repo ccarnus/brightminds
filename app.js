ab09be10b5dfc84a979ef36f9540616ed44844a3
Clément Carnus <clement.carnus@exotec.com>
2023-03-04 11:26:19 -0500

    implementation of a basic CRUD

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const mongoose = require('mongoose');
 
 const app = express();
 
+const Cast = require('./models/cast.js');
+
 mongoose.connect('mongodb+srv://ccarnus:totodu30@cast.xwxgb0o.mongodb.net/?retryWrites=true&w=majority')
     .then(() => {
         console.log('Succesully Connected to MongoDB Atlas!');
@@ -17,7 +19,6 @@ mongoose.connect('mongodb+srv://ccarnus:totodu30@cast.xwxgb0o.mongodb.net/?retry
         console.error(error);
     });
 
-app.use(express.json());
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
@@ -28,14 +29,85 @@ app.use((req, res, next) => {
 });
 
 app.post("/cast", (req, res, next) => {
-    res.status(200).json({
-        message:'received and saved'
+    const cast = new Cast({
+        title: req.body.title,
+        description: req.body.description,
+        department: req.body.department,
+        type: req.body.type,
+        brightmindid: req.body.brightmindid,
+        casturl: req.body.casturl,
+        universitylogourl: req.body.universitylogourl
+    });
+    cast.save().then(
+        () => {
+            res.status(201).json({response:'Cast Casted.'})
+        }
+    ).catch((error) => {
+        res.status(400).json({
+            error: error
+        });
     });
 });
 
 app.get("/cast", (req, res, next) => {
-    res.status(201);
-    res.json({message:'here is the cast'});
+    Cast.find().then(
+        (casts) => {
+            res.status(200).json(casts);
+        }
+    ).catch((error) => {
+        res.status(400).json({
+            error: error
+        });
+    });
+});
+
+app.get('/cast/:id', (req, res, next) => {
+    Cast.findOne({
+        _id:req.params.id
+    }).then(
+        (cast) => {
+            res.status(200).json(cast);
+        }
+    ).catch((error) => {
+        res.status(404).json({
+            error: error
+        });
+    });
+});
+
+app.put('/cast/:id', (req, res, next) => {
+    const cast = new Cast({
+        _id:req.params.id,
+        title: req.body.title,
+        description: req.body.description,
+        department: req.body.department,
+        type: req.body.type,
+        brightmindid: req.body.brightmindid,
+        casturl: req.body.casturl,
+        universitylogourl: req.body.universitylogourl
+    });
+    Cast.updateOne({_id:req.params.id}, cast)
+    .then(() => {
+        res.status(201).json({
+            response: "message updated"
+        })})
+    .catch((error) => {
+        res.status(400).json({
+            error: error
+        });
+    });
+});
+
+app.delete('/cast/:id', (req, res, next) => {
+    Cast.deleteOne({_id:req.params.id}).then(() => {
+        res.status(200).json({
+            response: 'Cast Deleted'
+        });
+    }).catch((error) => {
+        res.status(404).json({
+            error: error
+        });
+    })
 });
 
 module.exports = app;
