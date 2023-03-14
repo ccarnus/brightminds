ea2d57b0a5e9df568e068ea121daf3c1cfd561c1
Clément Carnus <clement.carnus@exotec.com>
2023-03-13 21:49:55 -0400

    adding stuff to the controller for the user

---
+++ backend/email_verificator.js
@@ -0,0 +1,6 @@
+function verifyEmail(email) {
+    var emailRegex = /^([a-zA-Z0-9._%-]+)@gatech.edu$/;
+    return emailRegex.test(email);
+  }
+
+module.exports.emailVerificator = verifyEmail;
--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,7 +1,14 @@
 const User = require('../models/user_model.js');
 const bcrypt = require('bcrypt');
+const emailVerificator = require('../backend/email_verificator.js');
+const jwt = require('jsonwebtoken');
 
 exports.signup = (req, res, next) => {
+    if(!emailVerificator(req.body.email)) {
+        return res.status(400).json({
+            error: "The email domain name id not a valid one."
+        });
+    }
     bcrypt.hash(req.body.password, 10).then(
         (hash) => {
             const user = new User({
@@ -32,5 +39,35 @@ exports.signup = (req, res, next) => {
 };
 
 exports.login = (req, res, next) => {
-    console.log('loginin');
+    User.findOne({email: req.body.email}).then(
+        (user) => {
+            if(!user) {
+                return res.status(401).json({
+                    response: 'user not found.'
+                });
+            }
+            bcrypt.compare(req.body.password,user.password).then(
+                (valid) => {
+                    if(!valid) {
+                        return res.status(401).json({
+                            response: 'incorrect password'
+                        });
+                    }
+                    const token = jwt.sign({userId: user._id});
+                    res.status(200).json({
+                        userId: user._id,
+                        email: user.email
+                    });
+                }
+            ).catch((error) => {
+                res.status(500).json({
+                    error: error
+                });
+            })
+        }
+    )
+};
+
+exports.getAllEmails = (req, res, next) => {
+
 };
