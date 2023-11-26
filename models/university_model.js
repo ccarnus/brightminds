36da813f8a4278f18a4a7009ca591badcb1411d9
Clément CARNUS <clement.carnus@exotec.com>
2023-11-26 16:31:50 -0500

    update will now update the university

--- models/university_model.js
+++ models/university_model.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const UniversitySchema = mongoose.Schema({
     name: {type:String, required:true},
-    score: {type:Number, required:true},
+    score: {type:Number, default:0},
     iconurl: {type:String, required:true}
 });
 
