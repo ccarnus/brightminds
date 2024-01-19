ac996e96be1add5dc5191310e4e384be66fd1821
Clément CARNUS <clement.carnus@exotec.com>
2024-01-18 20:08:06 -0500

    new backend folder

--- app.js
+++ app.js
@@ -33,6 +33,7 @@ app.use(bodyParser.json());
 
 app.use('/backend/media/cast_videos', express.static(path.join(__dirname,'/backend/media/cast_videos')));
 app.use('/backend/media/cast_images', express.static(path.join(__dirname,'/backend/media/cast_images')));
+app.use('/backend/media/article_images', express.static(path.join(__dirname,'/backend/media/article_images')));
 app.use('/backend/media/profile_pictures', express.static(path.join(__dirname,'/backend/media/profile_pictures')));
 app.use('/backend/media/university_icon', express.static(path.join(__dirname,'/backend/media/university_icon')));
 app.use('/backend/media/virtuallab_icon', express.static(path.join(__dirname,'/backend/media/virtuallab_icon')));
---
+++ backend/generate_article_image.js
@@ -0,0 +1,61 @@
+const openai = require('openai');
+const fs = require('fs');
+const path = require('path');
+const axios = require('axios');
+
+const apikey = process.env.OPENAI_API_KEY;
+const client = new openai({apikey});
+
+async function downloadImage(url, filepath) {
+    const writer = fs.createWriteStream(filepath);
+
+    const response = await axios({
+        url,
+        method: 'GET',
+        responseType: 'stream'
+    });
+
+    response.data.pipe(writer);
+
+    return new Promise((resolve, reject) => {
+        writer.on('finish', resolve);
+        writer.on('error', reject);
+    });
+}
+
+async function generateArticleImage(description) {
+    const modifiedDescription = `Create an engaging and visually appealing landscape cover image for an article. The content of the image should closely align with the following theme: ${description}. The design should be user-friendly, inviting, and accurately depict the article's subject matter. Don't make the image too dense, keep it simple, simple shapes.`;
+    try {
+        const response = await client.images.generate({
+            model: "dall-e-3",
+            prompt: description,
+            n: 1,
+            size: "1792x1024"
+        });
+
+        // Check if the response has the expected structure
+        if (!response.data || response.data.length === 0) {
+            console.error("Unexpected response structure:", response);
+            return null;
+        }
+
+        // Extract the image URL from the response
+        const imageUrl = response.data[0].url;
+
+        // Define the path where the image will be saved
+        const imageFileName = 'cast_' + Date.now() + '.jpg';
+        const imagePath = path.join(__dirname, '/media/article_images/', imageFileName);
+
+        // Download and save the image
+        await downloadImage(imageUrl, imagePath);
+
+        return imagePath;
+    } catch (error) {
+        console.error('Error generating article image:', error);
+        return null;
+    }
+}
+
+
+
+module.exports = generateArticleImage;
