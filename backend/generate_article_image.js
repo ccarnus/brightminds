ffd854ac57d62ffe7ef3cdd2e9788c16c9acd5f1
Clément CARNUS <clement.carnus@exotec.com>
2024-07-21 16:33:12 -0400

    change in DALL-E generation for articles

--- backend/generate_article_image.js
+++ backend/generate_article_image.js
@@ -24,11 +24,11 @@ async function downloadImage(url, filepath) {
 }
 
 async function generateArticleImage(description) {
-    const modifiedDescription =  `Create an engaging and visually appealing landscape cover image for an article. The design should be slick, simple, and user-friendly, inviting viewers without being too detailed. Focus on a few key components that accurately depict the video's subject matter, ensuring the image is clean and uncluttered.  The content of the image should align with the theme of a this description:  ${description}`;
+    const modifiedDescription = `Create an engaging and visually appealing landscape cover image for an article. The design should be slick, simple, and user-friendly, inviting viewers without being too detailed. Focus on a few key components that accurately depict the article's subject matter, ensuring the image is clean, attractive, and uncluttered. The image should be vibrant and eye-catching to attract readers. Do not include any text or writing in the image. The content of the image should align with the theme of this description: ${description}`;
     try {
         const response = await client.images.generate({
             model: "dall-e-3",
-            prompt: description,
+            prompt: modifiedDescription,
             n: 1,
             size: "1792x1024"
         });
@@ -43,7 +43,7 @@ async function generateArticleImage(description) {
         const imageUrl = response.data[0].url;
 
         // Define the path where the image will be saved
-        const imageFileName = 'cast_' + Date.now() + '.jpg';
+        const imageFileName = 'article_' + Date.now() + '.jpg';
         const imagePath = path.join(__dirname, '/media/article_images/', imageFileName);
 
         // Download and save the image
@@ -55,7 +55,5 @@ async function generateArticleImage(description) {
         return null;
     }
 }
-
-
 
 module.exports = generateArticleImage;
