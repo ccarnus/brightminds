422e43ec6fca983670f8f5b536aec21619558cb3
Clément CARNUS <clement.carnus@exotec.com>
2024-08-19 21:30:47 -0400

    changing the display of the track screen

--- backend/email_verificator.js
+++ backend/email_verificator.js
@@ -1,6 +1,6 @@
 function verifyEmail(email) {
-    var emailRegex = /^([a-zA-Z0-9._%-]+)@gatech.edu$/;
-    return emailRegex.test(email);
-  }
+  var emailRegex = /^([a-zA-Z0-9._%-]+)@(gatech\.edu|gmail\.com)$/;
+  return emailRegex.test(email);
+}
 
 module.exports = verifyEmail;
