92c985d5b74f543578f27099e9448835b8fecafb
Clément CARNUS <clement.carnus@exotec.com>
2025-01-11 15:39:48 -0500

    commiting

--- server.js
+++ server.js
@@ -1,7 +1,26 @@
+const https = require('https');
 const http = require('http');
+const fs = require('fs');
 const app = require('./app');
 
-app.set('port',process.env.PORT ||3000);
-const server = http.createServer(app);   
+const PORT = process.env.PORT || 3000;
 
-server.listen(process.env.PORT ||3000);
+const options = {
+  key: fs.readFileSync('/home/ubuntu/server.key'),
+  cert: fs.readFileSync('/home/ubuntu/server.crt')
+};
+
+const httpServer = http.createServer((req, res) => {
+    res.writeHead(301, { Location: `https://${req.headers.host}${req.url}` });
+    res.end();
+  });
+
+httpServer.listen(80, () => {
+console.log('HTTP server running, redirecting all traffic to HTTPS');
+});
+
+const server = https.createServer(options, app);
+
+server.listen(PORT, () => {
+  console.log(`Server is running on https://localhost:${PORT}`);
+});
